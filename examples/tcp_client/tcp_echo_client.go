366acca17b4f00c414ae5bd18ce121e20319fc59
Aviraj Khare <aviraj.khare@aux.go-jek.com>
2020-04-15 12:18:58 +0530

    [Aviraj] Add example code for tcp echo server and client

---
+++ examples/tcp_client/tcp_echo_client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	servAddr := "localhost:9010"
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", servAddr)
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	//send message
+	_, err = conn.Write([]byte("Hello"))
+	if err != nil {
+		println("Write to server failed:", err.Error())
+		os.Exit(1)
+	}
+
+	//receive buffer
+	buf := make([]byte, 1024)
+
+	//receive message
+	_, err = conn.Read(buf[0:])
+
+	if err != nil {
+		fmt.Printf("Got error")
+		os.Exit(1)
+	}
+	fmt.Printf("data received: %s", string(buf))
+}
---
+++ examples/tcp_server/tcp_echo_server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	serverAddr := "localhost:9010"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", serverAddr)
+	if err != nil {
+		fmt.Printf("Unable to resolve tcpAddr: %s", tcpAddr)
+	}
+
+	//listen for incoming connection
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Printf("Unable to accept: %s", err)
+	}
+
+	incomingMsg := make([]byte, 1024)
+
+	//receive message
+	_, err = conn.Read(incomingMsg)
+	if err != nil {
+		fmt.Printf("Unable to read message %s", err)
+	}
+
+	//send message back to client
+	_, err = conn.Write(incomingMsg)
+	if err != nil {
+		fmt.Printf("Unable to send message to client: %s", err)
+	}
+}
